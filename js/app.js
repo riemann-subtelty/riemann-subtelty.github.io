29cc1c1f675edc95ad1e79fc4747b5655b6feb22
Steve <sfk55@cornell.edu>
2016-08-18 03:00:52 -0400

    add storage

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ angular.module('app', ['firebase'])
             apiKey: "AIzaSyD0zGHLRsNUR144T_2bJxm_ZoFJJMqpDI0",
             authDomain: "sbrainstorming-e8084.firebaseapp.com",
             databaseURL: "https://sbrainstorming-e8084.firebaseio.com",
-            storageBucket: "",
+            storageBucket: "sbrainstorming-e8084.appspot.com",
         };
         firebase.initializeApp(config);
     });
